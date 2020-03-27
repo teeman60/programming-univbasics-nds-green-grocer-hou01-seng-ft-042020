42b76504babddfebc4a6337679b6d66e7386485d
Taiye Salami <taiye41@gmail.com>
2020-03-27 11:03:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -114,36 +114,50 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  new_cart = consolidate_cart(cart)
-  apply_coupons(new_cart, coupons)
-  apply_clearance(new_cart)
-  total = 0
-  i = 0
-  while i < new_cart.length do
-      item_num = new_cart[i][:count]
-      item_price = new_cart[i][:price].round(2)
-      total += (item_price * item_num)
-      i += 1
-  end
-    # item_price = consolidate_cart(cart)[i][:price]
-    # total = item_price * item_num
-    # # else
-    # #   ind = 0
-    #   while ind < apply_coupons(cart, coupons).length do
-    #     total = apply_coupons(cart, coupons)[ind][:price] * apply_coupons(cart, coupons)[ind][:count]
-    #     ind += 1
-    #   end
-    # end
-    # coupon_cart = apply_coupons(consolidate_cart(cart), [])
-    # if consolidate_cart(cart)[i][:clearance] == true
-    #     total = apply_clearance(cart)[i][:price]
-    # end
-    # total = apply_clearance[i][:price]
-
-if total > 100
-  total = (total-(total*0.1)).round(2)
-end
+#   new_cart = consolidate_cart(cart)
+#   apply_coupons(new_cart, coupons)
+#   apply_clearance(new_cart)
+#   total = 0
+#   i = 0
+#   while i < new_cart.length do
+#       item_num = new_cart[i][:count]
+#       item_price = new_cart[i][:price].round(2)
+#       total += (item_price * item_num)
+#       i += 1
+#   end
+#     # item_price = consolidate_cart(cart)[i][:price]
+#     # total = item_price * item_num
+#     # # else
+#     # #   ind = 0
+#     #   while ind < apply_coupons(cart, coupons).length do
+#     #     total = apply_coupons(cart, coupons)[ind][:price] * apply_coupons(cart, coupons)[ind][:count]
+#     #     ind += 1
+#     #   end
+#     # end
+#     # coupon_cart = apply_coupons(consolidate_cart(cart), [])
+#     # if consolidate_cart(cart)[i][:clearance] == true
+#     #     total = apply_clearance(cart)[i][:price]
+#     # end
+#     # total = apply_clearance[i][:price]
+# 
+# if total > 100
+#   total = (total-(total*0.1)).round(2)
+# end
   # binding.pry
   total
-
+  newcart=consolidate_cart(cart)
+  apply_coupons(newcart,coupons)
+  apply_clearance(newcart)
+  counter=0 
+  total=0 
+  while counter<newcart.length do 
+  total+=(newcart[counter][:price] * newcart[counter][:count]).round(2)
+  counter+=1 
+  end
+  
+  if total > 100
+   total= (total-(total*0.1)).round(2)
+  end
+  
+  total
 end
